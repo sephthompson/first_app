535079062bd724a452f88d7b9ff009c0bf9c0e2a
Seph Thompson <sephthompson@email.com>
2013-10-29 21:32:23 -0400

    Revert "Initialize repository"
    This reverts commit 24a68dc6dfe578038c125d17fed287aa7e7db964.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'edc7b98579a423f55ed72f5f480ac75aa613269a890508e7011be35b085f4501c8f11674fef4d898f3d60dd75b9583eceb94983e7a51e21d74445fe7fbb72ed7'
